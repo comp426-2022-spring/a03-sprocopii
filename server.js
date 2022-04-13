741bc5fada4c0af7840ba1d1d365c73fae1f7892
Stephan Procopii <stephanp@live.unc.edu>
2022-04-12 20:15:16 -0400

    added server script

---
+++ server.js
@@ -0,0 +1,26 @@
+//Require Express.js
+const express = require('express')
+const app = express()
+
+//Port variable
+var port = 5000
+
+//Start an app server
+const server = app.listen(port, () => {
+    console.log('App is running on port %PORT%'.replace('%PORT%', port))
+})
+
+//Default response for any other request (default endpoint)
+app.use(function(req, res) {
+    res.status(404).send('404 NOT FOUND')
+})
+
+//Check endpoint
+app.get('/app', (req, res) => {
+    //Respond with status 200
+    res.statusCode = 200
+    //Respond with status message "OK"
+    res.statusMessage = 'OK'
+    res.writeHead(res.statusCode, {'Content-Type' : 'text/plain'})
+    res.end(res.endStatusCode + ' ' + res.statusMessage)
+})
