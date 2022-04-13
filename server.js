d47073ab92069672a72d5ceb434c3db340472772
Stephan Procopii <stephanp@live.unc.edu>
2022-04-13 12:32:23 -0400

    test

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const app = express()
 const minimist = require('minimist')
-const args = ('minimist')(process.argv.slice(2))
+const args = minimist(process.argv.slice(2))
 
 //Port variable
 args['port']
